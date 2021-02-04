8d269a65e44454b5fa0ce47b98a8758778655f3a
Masum-Web-Developer <masum.diucse@gmail.com>
2021-02-04 23:25:24 +0600

    true vs false

---
+++ true.false.js
@@ -0,0 +1,16 @@
+//--falsy value
+// 0
+// ""
+// undefined
+//null
+//Nan
+//false
+
+//--truthy
+//'0',' ',[],{}
+let name={};
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
